b3454e469a48724fa1dc9d3f063b68b0aecb6f36
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-22 15:59:27 -0700

    futurerama api

--- twin_peaks/morbotron.rb
+++ twin_peaks/morbotron.rb
@@ -6,3 +6,24 @@ require 'httparty'
 account_sid = ENV["TWILIO_ACCOUNT_SID"] # Your Account SID from www.twilio.com/console
 auth_token = ENV["TWILIO_AUTH_TOKEN"] # Your Auth Token from www.twilio.com/console
 @client = Twilio::REST::Client.new account_sid, auth_token
+
+def get_quote
+  r = HTTParty.get('https://morbotron.com/api/random')
+  #check if our request had a valid response_body
+
+  if r.code == 200
+    json = r.parsed_response
+    #Extract the ep number and time stamp from API response.
+    _____, episode, timestamp = json["Frame"].values
+
+    #build a proper URL
+    image_url = "https://morbotron.com/meme" + episode + "/" + timestamp.to_s
+
+    # Combine each line of subtitles into one string, seperated by newlines.
+    caption = json["Subtitles"].map{|subtitle| subtitle["Content"]}.join("\n")
+
+    return image_url, caption
+  end
+end
+
+p get_quote
