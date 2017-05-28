e237104304e142b22724ac0667ad0bcac5c570c4
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-28 13:39:27 -0700

    app.rb

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 require 'twilio-ruby'
 
-post '/receive_sms' do
-  content_type 'text/xml'
-
-  response = Twilio::TwiML::Response.new do |r|
-    r.Message "Hey thanks for messaging me!"
-  end
-
-  response.to_xml
-end
+# post '/receive_sms' do
+#   content_type 'text/xml'
+#
+#   response = Twilio::TwiML::Response.new do |r|
+#     r.Message "Hey thanks for messaging me!"
+#   end
+#
+#   response.to_xml
+# end
 
 
 get '/' do
