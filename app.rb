4bc7a31df63fcfe5700d8d7404c65f600d4c3eac
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-30 23:56:49 -0700

    advice

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 
-# post '/receive_sms' do
-#   content_type 'text/xml'
-#
-#   response = Twilio::TwiML::Response.new do |r|
-#     r.Message "Hey thanks for messaging me!"
-#   end
-#
-#   response.to_xml
-# end
+post '/receive_sms' do
+  content_type 'text/xml'
+
+  response = Twilio::TwiML::Response.new do |r|
+    r.Message "Hey thanks for messaging me!"
+  end
+
+  response.to_xml
+end
 
 
 get '/' do
